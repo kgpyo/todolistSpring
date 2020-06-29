6d74d8574da51a56fb1bcf2843fae4dd98014074
kgpyo <kgpyo123@gmail.com>
2020-06-29 23:32:19 +0900

    feat: remve duplicate generate link method
    - 중복되는 링크생성(selfRef)를 하나의 메소드로 구현하기 위해
    RepresentiaonModelAssembler 인터페이스 구현
    - 인터페이스 사용을 위해 의존성 주입

--- todoslitApi/src/main/java/com/example/todoslitApi/payroll/EmployeeController.java
+++ todoslitApi/src/main/java/com/example/todoslitApi/payroll/EmployeeController.java
@@ -11,17 +11,16 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 @RestController
 public class EmployeeController {
     private final EmployeeRepository repository;
+    private final EmployeeModelAssembler assembler;
 
-    EmployeeController(EmployeeRepository repositry) {
+    EmployeeController(EmployeeRepository repositry, EmployeeModelAssembler assembler) {
         this.repository = repositry;
+        this.assembler = assembler;
     }
 
     @GetMapping("/employees")
     CollectionModel<EntityModel<Employee>> all() {
-        List<EntityModel<Employee>> employees = repository.findAll().stream()
-                .map(employee -> EntityModel.of(employee,
-                        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
-                        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+        List<EntityModel<Employee>> employees = repository.findAll().stream().map(assembler::toModel)
                 .collect(Collectors.toList());
 
         return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
@@ -37,8 +36,7 @@ public class EmployeeController {
 
         Employee employee = repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
 
-        return EntityModel.of(employee, linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-                linkTo(methodOn(EmployeeController.class).all()).withRel("Employees"));
+        return assembler.toModel(employee);
     }
 
     @PutMapping("/employees/{id}")
