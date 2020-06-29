9d61be3edd5d9c1b53379f6d528515522eb66d0e
kgpyo <kgpyo123@gmail.com>
2020-06-29 15:36:36 +0900

    init: generate spring boot

---
+++ todoslitApi/src/main/java/com/example/todoslitApi/TodoslitApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.todoslitApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoslitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoslitApiApplication.class, args);
+	}
+
+}
---
+++ todoslitApi/src/test/java/com/example/todoslitApi/TodoslitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoslitApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoslitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
