bc92b2f630679c463841eeb2d04a2bb558a56212
kgpyo <kgpyo123@gmail.com>
2020-06-29 16:16:55 +0900

    feat: add crud controller for emplyoee
    - 생성자를 통해 의존성 주입

---
+++ todoslitApi/src/main/java/com/example/todoslitApi/payroll/EmployeeController.java
@@ -0,0 +1,41 @@
+package com.example.todoslitApi.payroll;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class EmployeeController {
+    private final EmployeeRepository repository;
+
+    EmployeeController(EmployeeRepository repositry) {
+        this.repository = repositry;
+    }
+
+    @GetMapping("/employees")
+    List<Employee> all() {
+        return repository.findAll();
+    }
+
+    @PostMapping("/employees")
+    Employee newEmployee(@RequestBody Employee newEmployee) {
+        return repository.save(newEmployee);
+    }
+
+    @PutMapping("/employees/{id}")
+    Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+        return repository.findById(id).map(employee -> {
+            employee.setName(newEmployee.getName());
+            employee.setRole(newEmployee.getRole());
+            return repository.save(employee);
+        }).orElseGet(() -> {
+            newEmployee.setId(id);
+            return repository.save(newEmployee);
+        });
+    }
+
+    @DeleteMapping("/employees/{id}")
+    void deleteEmployee(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
